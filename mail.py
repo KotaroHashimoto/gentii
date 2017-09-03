82a234ca41e0864c1051ee5594c47f35cf6e6d51
Kotaro Hashimoto <hasimoto.kotaro@gmail.com>
2017-09-03 16:26:16 +0900

    m
    Signed-off-by: Kotaro Hashimoto <hasimoto.kotaro@gmail.com>

--- mail.py
+++ mail.py
@@ -9,9 +9,8 @@ from email.utils import formatdate
 import smtplib
 
 if __name__ == "__main__":
-    # åæをåç
+
     section, to, subject, body = sys.argv[1:5]
-    # ini ファイルのデフォルトåをèå
     defaults = {
         'host': '127.0.0.1', 'port': 25,
         'use_smtps': 'no',
@@ -20,7 +19,7 @@ if __name__ == "__main__":
         'from': 'pandora@localhost',
         'encoding': 'iso-2022-jp',
     }
-    # ini ファイルをèみèむ
+
     config = SafeConfigParser(defaults)
     conf_path = os.path.join(os.path.dirname(__file__), 'mail.ini')
     config.read(conf_path, encoding = 'utf-8')
@@ -29,13 +28,13 @@ if __name__ == "__main__":
         c[key] = value
     for key in ['use_smtps', 'use_starttls']:
         c[key] = config.getboolean(section, key)
-    # email メッセージのäæ
+
     msg = MIMEText(body, 'plain', c['encoding'])
     msg['Subject'] = Header(subject, c['encoding'])
     msg['From'] = c['from']
     msg['To'] = to
     msg['Date'] = formatdate(localtime=True)
-    # 送信
+
     if c['use_smtps']:
         smtp = smtplib.SMTP_SSL(c['host'], c['port'])
     else:
