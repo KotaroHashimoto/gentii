9232648ddbba578da5f2a2292223a42e102e00bd
Kotaro Hashimoto <hasimoto.kotaro@gmail.com>
2017-10-09 23:13:26 +0900

    a
    Signed-off-by: Kotaro Hashimoto <hasimoto.kotaro@gmail.com>

--- btcLev_CC-QN.py
+++ btcLev_CC-QN.py
@@ -2,21 +2,25 @@
 # -*- coding: utf-8 -*-
 
 # QUOINEX / Coincheck がこの数値％以上となったら QUOINEX BTC売り、Coincheck BTC買い
-SellQN_BuyCC_Percentage = 1
+SellQN_BuyCC_Percentage = 0.1
 
 # QUOINEX / Coincheck がこの数値％以下となったら QUOINEX BTC売り、Coincheck BTC買い ポジションのクローズ
 Close_SellQN_BuyCC_Percentage = 0
 
 # QUOINEX / Coincheck がこの数値％以下となったら QUOINEX BTC買い、Coincheck BTC売り
-BuyQN_SellCC_Percentage = -1
+BuyQN_SellCC_Percentage = -0.1
 
 # QUOINEX / Coincheck がこの数値％以上となったら QUOINEX BTC買い、Coincheck BTC売り ポジションのクローズ
 Close_BuyQN_SellCC_Percentage = 0
 
-
 # １回に取引するBTCの枚数
 BTC_Trade_Amount = 1.0
 
+# Quoineのレバレッジレベル(2, 4, 5, 10, 25)
+Quoine_Leverage = 25
+
+# 売買を実行した後にスリープする時間[秒]
+Mask_After_Trade_Sec = 10
 
 # QUOINEX トークンID
 QN_Key = ''
@@ -34,16 +38,15 @@ CC_Secret = ''
 
 from datetime import datetime
 import pybitflyer
-from json import loads, dump
+from json import loads, dumps
 import ssl
 from math import floor
 import sys
-import json
 import requests
 import time
 import hmac
 import hashlib
-
+import jwt
 
 
 class Quoine():
@@ -92,16 +95,18 @@ class Quoine():
         order['order_type'] = 'market'
         order['product_id'] = 5 #BTCJPY
         order['side'] = side
-        order['quantity'] = amount
-        order['leverage_level'] = 1
+        order['quantity'] = str(amount)
+        order['leverage_level'] = Quoine_Leverage
         order['price'] = str(self.bid[0] if side == 'sell' else self.ask[0])
         order['order_direction'] = 'netout'
         order['currency_pair_code'] = 'BTCJPY'
+        order['funding_currency'] = 'JPY'
         order['product_code'] = 'CASH'
 
         path = '/orders/'
         body = dumps(order)
-        timestamp = str(int(time()))
+        timestamp = str(int(time.time()))
+#        print(timestamp)
         auth_payload = {
             'path': path,
             'nonce': timestamp,
@@ -109,6 +114,7 @@ class Quoine():
             }
         sign = jwt.encode(auth_payload, Quoine.api_secret, algorithm='HS256')
 
+#        print(body)
 
         response = requests.post(
             Quoine.api_endpoint+path
@@ -119,16 +125,18 @@ class Quoine():
                 'Content-Type': 'application/json'
                 })
 
+        return response
+
 #        print(response.status_code)
 #        print(response.text)
 #        print(response.headers)
 
 
     def sell(self, am):
-        self.orderQuoine('sell', am)
+        return self.orderQuoine('sell', am)
 
     def buy(self, am):
-        self.orderQuoine('buy', am)
+        return self.orderQuoine('buy', am)
 
 
 class CCApiCall:
@@ -153,7 +161,9 @@ class CCApiCall:
         return request_data
 
     def post_api_call(self,path,body):
-        body = json.dumps(body)
+        body = dumps(body)
+
+#        print('okure', body)
         timestamp = str(int(time.time()))
         text = timestamp + self.api_endpoint + path + body
         sign = hmac.new(bytes(self.api_secret.encode('ascii')), bytes(text.encode('ascii')), hashlib.sha256).hexdigest()
@@ -191,6 +201,8 @@ class CC:
         side = ''
         amount = -1
 
+#       print(res)
+
         if 0 < len(res['data']):
             id = res['data'][0]['id']
             side = res['data'][0]['side']
@@ -224,7 +236,7 @@ class CC:
                 'position_id': id
                 }
 
-        CC.private.post_api_call(CC.ORDER, body).json()
+        return CC.private.post_api_call(CC.ORDER, body).json()
         
 
     def buy(self, id, side, am):
@@ -244,7 +256,7 @@ class CC:
                 'position_id': id
                 }
 
-        CC.private.post_api_call(CC.ORDER, body).json()
+        return CC.private.post_api_call(CC.ORDER, body).json()
 
         
 class Position:
@@ -326,13 +338,13 @@ if __name__ == '__main__':
 
                 if op == 'Sell Quoine' and cc_side != 'buy':
                     print('\nSell Quoine BTC, Buy Coincheck BTC: ' + str(amount)  + '\n')
-                    print(cc.buy(id, side, cc_amount))
+                    print(cc.buy(id, cc_side, cc_amount))
                     print(qn.sell(BTC_Trade_Amount))
                     time.sleep(Mask_After_Trade_Sec)
 
                 if op == 'Buy Quoine' and cc_side != 'sell':
                     print('\nBuy QUoine BTC, Sell Coincheck, BTC: ' + str(amount)  + '\n')
-                    print(cc.sell(id, side, cc_amount))
+                    print(cc.sell(id, cc_side, cc_amount))
                     print(qn.buy(BTC_Trade_Amount))
                     time.sleep(Mask_After_Trade_Sec)
 
